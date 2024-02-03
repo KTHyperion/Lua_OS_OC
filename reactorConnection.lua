2a9553dd3fd64e464e618ded12f70425f0f2ccce
Giovani Camargo <giovanicamargo99@gmail.com>
2024-02-03 01:11:58 -0300

    Adjusted variable declaration bugs.

--- reactorConnection.lua
+++ reactorConnection.lua
@@ -4,11 +4,10 @@ local gpuSerial = "<SERIAL GPU>"
 local monitorSerial = "<SERIAL MONITOR>"
 local reactorSerial = "<SERIAL REATOR>"
 
-local monitor = component.proxy(gpuSerial)
+monitor = component.proxy(gpuSerial)
 monitor.bind(monitorSerial)
-local xLength, yHeight = monitor.getResolution()
 
-local re = component.proxy(reactorSerial)
+re = component.proxy(reactorSerial)
 
 -- reactorGraphics will further work with both monitor and reactor's data.
 os.execute("reactorGraphics monitor re")
--- reactorGraphics.lua
+++ reactorGraphics.lua
@@ -1,5 +1,15 @@
+local re = re
+local monitor = monitor
+
 local conReactor = re.getConnected()
 local statusReactor = re.getActive()
+
+local xLength, yHeight = monitor.getResolution()
+
+-- Remember to set both background and foreground colors as you wish before calling this function
+function clear ()
+    monitor.fill(1, 1, xLength, yHeight, " ")
+end
 
 if conReactor == true then
     rstatus = "Conectado"
