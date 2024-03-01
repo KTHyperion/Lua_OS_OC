e3814af7ae3e6e3c1bd69adbbefc43d83d299489
Giovani Camargo <giovanicamargo99@gmail.com>
2024-03-01 03:31:15 -0300

    Added sliders

--- reactorGraphics.lua
+++ reactorGraphics.lua
@@ -19,23 +19,25 @@ function setColorBF (color1, color2)
     monitor.setForeground(color2)
 end
 
-if conReactor == true then
-    rconnection = "Connected"
-else
-    rconnection = "Not connected"
-end
+function reInfo ()
+    if conReactor == true then
+        rconnection = "Connected"
+    else
+        rconnection = "Not connected"
+    end
 
-if statusReactor == true then
-    act = "Active"
-else
-    act = "Inactive"
+    if statusReactor == true then
+        act = "Active"
+    else
+        act = "Inactive"
+    end
+
+    monitor.set(1, 1, "Reactor connection: "..rconnection)
+    monitor.set(1, 2, "Reactor status: "..act)
 end
 
 setColorBF(0x000000, 0xFFFFFF)
 clear()
-
-monitor.set(1, 1, "Reactor connection: "..rconnection)
-monitor.set(1, 2, "Reactor status: "..act)
 
 monitor.set(1, 4, "Internal buffer")
 setColorBF(0xFFFFFF, 0x000000)
@@ -47,4 +49,4 @@ local internalEnergy = re.getEnergyStored()
 local proportion = internalEnergy / reactorMaxEnergy
 
 setColorBF(0xFF0000, 0x000000)
-monitor.fill(2, 6, 2+proportion, 6, " ")
+monitor.fill(2, 6, ((xLength - 2) * proportion), 6, " ")
